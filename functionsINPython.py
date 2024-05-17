8d903d2a974fdd1bcbf79495990545abf1ab0919
SWAPNEEL PUROHIT <rsnikupurohit17@gmail.com>
2024-05-17 08:55:07 +0530

    python programming 16/05/2024

---
+++ functionsINPython.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    print("THE SUM OF THE PROVIDED NUMBER IS :  "+str(x+y))
+    return
+
+flag_1=int(input("ENTRE THE FIRST NUMBER:   "))
+
+flag_2=int(input("ENTRE THE SECOEND NUMBER:   "))
+add(flag_1,flag_2)
---
+++ whileBAsic.py
@@ -0,0 +1,8 @@
+
+    
+n = int(input("ENTRE THE VALUE OF N :"))
+i=0
+while i<=n:
+    j=i**2
+    print(str(j))
+    i=i+1
