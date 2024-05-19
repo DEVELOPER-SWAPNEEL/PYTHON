52aa77400bbadd20612b17db8a8cbce20e1cc1a1
SWAPNEEL PUROHIT <rsnikupurohit17@gmail.com>
2024-05-19 13:26:29 +0530

    Dictionaries

---
+++ DATA_STRUCTURES_AND_STRINGS/1_dictionaries.py
@@ -0,0 +1,11 @@
+# list --- > [] , index value
+# list = [1,2,3,4,5]
+
+# dictionary --- > {} , key
+# dictionary = {key:value, key:value ..... }
+
+my_dict={'key1':1,'key2':'2','key3':[3212],'key4':(432),'key5':5}
+print(my_dict['key2'])#here to call that particuler dictonary we need to put square brackets..
+
+dictionary = {1:'a',2:'b',3:'c'}
+print(dictionary[2])
