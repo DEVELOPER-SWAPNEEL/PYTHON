ad18ade4a67bf35646af7d86d943660766de0b25
SWAPNEEL PUROHIT <rsnikupurohit17@gmail.com>
2024-05-19 13:24:08 +0530

    FILES_EXCEPTIONS_AND_ERRORS

---
+++ FILES_EXCEPTIONS_AND_ERRORS/FILE_HANDELING_(OPENDING_AND_CLOSEING).py
@@ -0,0 +1,9 @@
+#1
+file_1=open('my_file.txt','r')
+#statements
+file_1.close()
+'''HERE IN THIS METHOD 1 YOU HAVE TO CLOSE THE OPENED FILES'''
+#2
+#with open('my_file.txt','w' as file_1):
+#statements
+'''NO SPECIAL NEED TO CLOSE THE TEXT IF OPENED IN THIS SECOND FORMAT..'''
---
+++ FILES_EXCEPTIONS_AND_ERRORS/reading_data_from_a_file.py
@@ -0,0 +1,5 @@
+
+file_1= open('my_file.txt','r')
+reading_file=file_1.read()
+print(reading_file)
+file_1.close()
